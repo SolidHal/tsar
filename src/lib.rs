958bd2bab5f54298b9d1047d5d00aa80f7812e0d
Eva Emmerich <eva@evaemmerich.com>
2025-05-19 15:39:37 -0500

    implement find_album_tracks

--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,50 @@ mod controlclient;
 pub async fn tsar_run(_output_dir: PathBuf, uri: &String, cache_dir: PathBuf, _username: &String, _empty_playlist: u8) {
     let spotify_api = controlclient::create_playback_client(cache_dir).await;
 
-    let _tracks: Vec<FullTrack>  = find_playlist_tracks(&spotify_api, uri).await;
+    let tracks: Vec<FullTrack>;
+
+    if uri_is_playlist(uri){
+        tracks  = find_playlist_tracks(&spotify_api, uri).await;
+        print_tracks(&tracks);
+    }
+    else if uri_is_album(uri) {
+        tracks  = find_album_tracks(&spotify_api, uri).await;
+        print_tracks(&tracks);
+    }
+
+    else {
+        panic!("Unable to handle uri {uri}. uri should be for an album <spotify:album:blah> or a playlist <spotify:playlist:blah>");
+    }
 
 
+}
+
+fn uri_is_playlist(uri: &String) -> bool {
+    if uri.contains("spotify:playlist"){
+        return true;
+    }
+    return false;
+}
+
+fn uri_is_album(uri: &String) -> bool {
+    if uri.contains("spotify:album"){
+        return true;
+    }
+    return false;
 }
 
 fn get_track_from_playable(item: &PlayableItem) -> &track::FullTrack {
     match item {
         PlayableItem::Track(t) => t,
         _ => panic!("Unable to handle episodes yet"),
+    }
+}
+
+fn print_tracks(tracks: &Vec<FullTrack>){
+    for track in tracks{
+        let name = &track.name;
+        let uri = track.id.as_ref().unwrap().id();
+        println!("track {name} {uri}");
     }
 }
 
@@ -34,12 +69,29 @@ async fn find_playlist_tracks(spotify_api: &AuthCodeSpotify, uri: &String) -> Ve
         let tmp_playable = item.track.unwrap();
         let track = get_track_from_playable(&tmp_playable);
 
-        let name = &track.name;
-        let uri = tmp_playable.id().expect("failed to get tracks id/uri");
-        println!("found track {name} {uri:?}");
-
         tracks.push(track.clone());
     }
 
     return tracks
+}
+
+async fn find_album_tracks(spotify_api: &AuthCodeSpotify, uri: &String) -> Vec<FullTrack> {
+    println!("getting all tracks from playlist {uri}");
+    // Get all the tracks from the album
+    let album_uri = AlbumId::from_id_or_uri(uri).expect("unable to create album object from uri, is uri valid?");
+
+    // album_track gives us SimplifiedTrack objects, but playlist_items gives us FullTrack objects
+    // convert all SimplifiedTrack objects by asking for each track in the album by uri
+    let album_paginator = spotify_api.album_track(album_uri, Option::from(Market::FromToken));
+    let album_items = album_paginator.try_collect::<Vec<_>>().await.expect("failed to get all tracks from album");
+
+    let mut album_uris: Vec<TrackId> = Vec::<TrackId>::new();
+    for item in album_items {
+        let uri = item.id.expect("failed to get album tracks id/uri");
+        album_uris.push(uri);
+    }
+
+    let album_tracks = spotify_api.tracks(album_uris, Option::from(Market::FromToken)).await.unwrap();
+
+    return album_tracks
 }
